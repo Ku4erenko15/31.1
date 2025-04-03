b8bf88bd83f953b8d0654dbbc53f56bd96ec6437
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-04-03 11:15:50 +0300

    Add files via upload

---
+++ 31.1/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31._1
+{
+    class Rectangle : Figure 
+    {
+        private double side1, side2;
+
+        public double Side1 { get { return side1; } set { side1 = value; } }
+        public double Side2 { get { return side2; } set { side2 = value; } }
+
+        public Rectangle(double side1, double side2) : base("Прямоугольник")
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        public override double Area() => side1 * side2;
+        public override double Perimeter() => (side2 + side1) * 2;
+
+        public override void Print()
+        {
+            base.print();
+            Console.WriteLine($"стороны:{side1} и {side2}. Площадь:{Area()}; Периметр:{Perimeter()}");
+        }
+    }
+}
---
+++ 31.1/Square.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31._1
+{
+     class Square: Figure
+    {
+        private double side1;
+
+        public double Side1 { get { return side1; } set { side1 = value; } }
+        
+
+        public Square(string name, double side1) : base(name)
+        {
+            this.side1 = side1;
+            
+        }
+
+        public override double Area() => side1 * side1;
+        public override double Perimeter() => side1 * 4;
+
+        public override void Print()        
+        {
+            base.Print();
+            Console.WriteLine($"Сторона:{side1}. Площадь:{Area()}; Периметр:{Perimeter()}");
+        }
+        public static Square Enter()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            string name = Console.ReadLine();
+            Console.WriteLine("");
+            double side1 = double.Parse(Console.ReadLine());
+            return new Square (name, side1);
+        }
+    }
+}
